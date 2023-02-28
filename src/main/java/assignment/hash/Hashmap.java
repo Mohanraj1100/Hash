991ac75294a8af1b3a656113eef81fa2b474be74
Mohanraj1100 <123471382+Mohanraj1100@users.noreply.github.com>
2023-02-28 11:34:49 +0530

    commit

--- src/main/java/assignment/hash/Hashmap.java
+++ src/main/java/assignment/hash/Hashmap.java
@@ -1,5 +1,6 @@
 package assignment.hash;
 import java.util.*;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 class Hashmap 
@@ -7,22 +8,21 @@ class Hashmap
     void hashMap()
     {
         HashMap<String,String> has = new HashMap<String,String>();
-        Logger log = Logger.getLogger("hi");
+        Logger l = Logger.getLogger("hi");
         has.put("Vaigai EXP", "06:30");
         has.put("Pandian EXP","07:00");
         has.put("Chennai EXP","19:00");
-        log.info("Entries are"+String.valueOf(has));
-        log.info(String.valueOf(has.get("Vaigai EXP")));
+        l.log(Level.INFO,()-> "Entries are"+ has);
         HashMap<String,String> has2 = new HashMap<String,String>();
         has2.putAll(has);
-        log.info("Values after adding another hashmap values"+String.valueOf(has2));
-        log.info("Size is "+String.valueOf(has2.size()));
+        l.log(Level.INFO,()->"Values after adding another hashmap values"+has2);
+        l.log(Level.INFO,()->"Size is "+has2.size());
         Set<Map.Entry<String, String>> s = has.entrySet();
-        log.info("Print the keys"+String.valueOf(has.keySet()));
-        log.info("Print the values"+String.valueOf(has.values()));
+        l.log(Level.INFO,()->"Print the keys"+has.keySet());
+        l.log(Level.INFO,()->"Print the values"+has.values());
         has2.clear();
-        log.info("Values after cleared"+String.valueOf(has2));
-        log.info("Set is"+String.valueOf(s));
+        l.log(Level.INFO,()->"Values after cleared"+has2);
+        l.log(Level.INFO,()->"Set is"+s);
         
         
         
