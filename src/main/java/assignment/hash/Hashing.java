b355964b18b8c43f5df15df7a9b237fe253a7123
Mohanraj1100 <123471382+Mohanraj1100@users.noreply.github.com>
2023-02-28 11:34:01 +0530

    commit

--- src/main/java/assignment/hash/Hashing.java
+++ src/main/java/assignment/hash/Hashing.java
@@ -3,7 +3,7 @@ package assignment.hash;
 import java.util.Scanner;
 import java.util.logging.Logger;
 
-public class Hashing {
+public class Hashing {  
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
